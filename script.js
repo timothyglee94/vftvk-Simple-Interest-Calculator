0f64dc5e45c1f238ec89d4ee12041ab83e4eaf83
Timothy <timothyglee94@gmail.com>
2021-02-05 23:11:36 +0000

    save for local use

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principle = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principle*years*rate/100
     
+    document.getElementById("rate").onchange = function(){rateChanger()};
+
+    function rateChanger(){
+        var rate = document.getElementById("rate").value;
+        span.innerText(rate);
+    }
 }
         
