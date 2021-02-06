7b1d717568c9f41d1a9fdfdd93ff54b9ec65f818
Timothy <timothyglee94@gmail.com>
2021-02-05 16:49:26 -0800

    final version

--- script.js
+++ script.js
@@ -4,13 +4,36 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
-    var interest = principle*years*rate/100
-    
-    document.getElementById("rate").onchange = function(){rateChanger()};
-
-    function rateChanger(){
-        var rate = document.getElementById("rate").value;
-        span.innerText(rate);
+    /*validation check */ 
+    if(principle < "1")
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
     }
+    /*calulating interest */
+
+    var interest = principle*years*rate/100;
+    
+    /*calculating correct year*/
+    var year = parseInt(years)+2021;
+
+    /*binding the result*/
+    var result = document.getElementById("result");
+
+    result.innerHTML = "<br/>If you deposit <mark>" + principle +
+    "</mark>, <br/>at an intertest rate of <mark>" + rate + "%</mark>." + 
+    "<br/>You will recieve an amount of <mark>" + interest + "</mark>," + 
+    "<br/>in year <mark>" + year +"</mark>";
+
 }
-        
+
+/* onchange when user moves range slider*/
+window.onload = function() {
+    document.getElementById("rate").onchange = function(){rateChanger()};
+}
+
+function rateChanger(){
+    var rate = document.getElementById("rate").value;
+    document.getElementById("intrate").textContent = rate;
+}
